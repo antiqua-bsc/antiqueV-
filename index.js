0d8a63a515bbeb1eaef62abe6b589e4831b1c259
antiqua-bsc <antiqua.bsc@gmail.com>
2022-04-25 23:29:41 +0000

    new changes

--- index.js
+++ index.js
@@ -2,18 +2,9 @@ const mobile_header = document.querySelector("#mobile_header");
 const mobile_header_trigger = document.querySelector("#mobile_header_trigger");
 const mobile_header_close = document.querySelector("#mobile_header_close");
 const nfts = document.querySelectorAll(".nft");
-const nft_container = document.querySelector("#nft_container");
 
 var nft_interval;
 var currentNft = 0;
-
-nft_container.addEventListener("mouseenter", () => {
-  clearInterval(nft_interval);
-});
-
-nft_container.addEventListener("mouseleave", () => {
-  suffleNfts();
-});
 
 const suffleNfts = () => {
   nft_interval = setInterval(() => {
